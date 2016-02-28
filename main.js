2570a5d19355c9e85fa08aa4d0ceeb2e4ca93827
joshua-smith88 <joshua.smith@massfinity.com>
2016-02-28 12:52:30 -0500

    added main.js back to project

---
+++ main.js
@@ -0,0 +1,62 @@
+var settings = require('_Settings');
+
+var factory = require('creepFactory');
+var roles = require('creepRoles');
+
+var harvester = require('harvestStuff');
+var buildStuff = require('buildStuff');
+var guardStuff = require('guardStuff');
+
+
+module.exports.loop = function () {
+    for(var r in Game.rooms) {
+        var _room = Game.rooms[r];
+        var _creeps = _room.find(FIND_CREEPS);
+        var _spawns = _room.find(FIND_MY_SPAWNS);
+        var _sources = _room.find(FIND_SOURCES);
+        var _extensions = [];
+        var _towers = [];
+        var _storages = [];
+        
+        var structs = _room.find(FIND_STRUCTURES);
+        for(var i in structs) {
+            if (structs[i].structureType == STRUCTURE_EXTENSION)
+                _extensions.push(structs[i]);
+            if (structs[i].structureType == STRUCTURE_TOWER)
+                _towers.push(structs[i]);
+            if (structs[i].structureType == STRUCTURE_STORAGE)
+                _storages.push(structs[i]);
+        }
+        _room.memory.harvesterCount = 0;
+        _room.memory.builderCount = 0;
+        _room.memory.guardCount = 0;
+        
+        for(var i in _creeps) {
+            if(_creeps[i].memory.role) {
+                switch(_creeps[i].memory.role.value) {
+                    case roles.HARVESTER.value:
+                        _room.memory.harvesterCount++;
+                        harvester.Work(_creeps[i], _room, _spawns, _extensions, _towers, _storages);
+                        break;
+                    case roles.BUILDER.value:
+                        _room.memory.builderCount++;
+                        buildStuff(_creeps[i], _room.controller, _extensions);
+                        break;
+                    case roles.GUARD.value:
+                        _room.memory.guardCount++;
+                        guardStuff(_creeps[i]);
+                        break;
+                }
+            }
+        }
+        
+        //remove old resources
+        for(var i in Memory.creeps) {
+            if(!Game.creeps[i]) {
+                delete Memory.creeps[i];
+            }
+        }
+        
+        factory.ProcessQueue(_room, _spawns, _sources);
+    }
+}
